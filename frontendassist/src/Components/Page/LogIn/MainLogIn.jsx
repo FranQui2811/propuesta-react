afe9ea397680050ec534164b1e89cc0768e60345
Miguel Ibarra <miguelgoo205@gmail.com>
2025-11-16 17:20:47 -0500

    Conexion con endpoint.M.I

--- frontendassist/src/Components/Page/LogIn/MainLogIn.jsx
+++ frontendassist/src/Components/Page/LogIn/MainLogIn.jsx
@@ -1,29 +1,101 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import './MainLogIn.css';
-import logo from '../../../images/logo.png'
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+import Swal from 'sweetalert2'
+import { UserDataContext } from '../../Page/Context/UserDataProvider';
+
 
 
 export const MainLogIn = () => {
-  const [formData, setFormData] = useState({
+  const {updateUserData} = useContext(UserDataContext);
+  const navigate = useNavigate();
+
+      // Ventanas de validación 
+    const incorrect = () =>{
+        Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: 'Verifica los datos',
+          })
+    }
+    const correct = () => {
+      let timerInterval;
+      Swal.fire({
+        icon: "success",
+        title: 'Bienvenido',
+        timer: 1000,
+        timerProgressBar: true,
+        didOpen: () => {
+          Swal.showLoading();
+          const b = Swal.getPopup().querySelector('b');
+          if (b) {
+            timerInterval = setInterval(() => {
+              b.textContent = Swal.getTimerLeft();
+            }, 100);
+          }
+        },
+        willClose: () => {
+          clearInterval(timerInterval);
+        }
+      }).then((result) => {
+        if (result.dismiss === Swal.DismissReason.timer) {
+          // Lógica adicional después de que se cierre la alerta
+        }
+      });
+    };
+
+  const [inputs, setInputs] = useState({
     email: '',
     password: ''
   });
+  const { email, password } = inputs;
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData(prev => ({ ...prev, [name]: value }));
+  const onChange = (e) => {
+    setInputs({...inputs, [e.target.name]: e.target.value})
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Login attempt with:', formData);
+  const getData = async(e) =>{
+    e.preventDefault()
+    const Usuario = {
+    email
   };
+      
+    try {
+      const response = await axios.post("https://backend-space-parking.onrender.com/api/users/rolUser", Usuario);
+      let userRole = response.data.roles
+      const typerole = userRole.slice(-1)
+      onSubmitLogin(typerole)
+    } catch (error) {
+      incorrect()
+    }
+    }
+
+      const onSubmitLogin = async(typerole) => {
+
+        const Usuario = {
+          email, password
+        };
+       
+        try {
+            const response = await axios.post("https://backend-space-parking.onrender.com/api/users/login", Usuario);
+            updateUserData(response.data);
+            if (typerole === '5') {
+              navigate(`/HomeUser/`);
+            }else {
+              navigate(`/HomeParking/`);
+            }
+            correct()
+          } catch (error) {
+            incorrect()
+          }
+        };
 
   return (
   <>
     {/* 🔹 Logo global, fuera del contenedor */}
     <div className="page-logo">
-      <img src={logo} alt="Logo" />
+      <img src='https://res.cloudinary.com/miguelgo205/image/upload/v1763329833/ListUr/LogoT.png' alt="Logo" />
     </div>
 
     {/* 🔹 Contenedor principal del login */}
@@ -32,14 +104,14 @@ export const MainLogIn = () => {
 
       <div className="login-box">
         <h2>Bienvenido</h2>
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={(e) => getData(e)}>
           <div className="form-group">
             <label htmlFor="email">Correo electrónico</label>
-            <input type="email" id="email" name="email" />
+            <input onChange={(e) => onChange(e)} type="email" id="email" name="email" required />
           </div>
           <div className="form-group">
             <label htmlFor="password">Contraseña</label>
-            <input type="password" id="password" name="password" />
+            <input onChange={(e) => onChange(e)} type="password" id="password" name="password" required />
           </div>
           <button type="submit">Ingresar</button>
         </form>
